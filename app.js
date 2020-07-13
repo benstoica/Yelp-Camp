26ddd1065c29752ee815d9a5a63f7576a3ce34c2
Ben Stoica <bennystoica@gmail.com>
2020-07-12 21:17:34 -0400

    styling campgrounds page

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ app.set('view engine', 'ejs');
 const campgrounds = 
 [
     {name: "Salmon Creek", image:"https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Granite Hill", image:"https://images.pexels.com/photos/1840421/pexels-photo-1840421.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350"}
+    {name: "Granite Hill", image:"https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440712a78d09e48c0_340.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Salmon Creek", image:"https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Granite Hill", image:"https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440712a78d09e48c0_340.jpg"},
+    {name: "Salmon Creek", image:"https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Granite Hill", image:"https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440712a78d09e48c0_340.jpg"}
 ];
 
 app.get('/', function(req, res){
