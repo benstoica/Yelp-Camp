87a5450a4a9a4b47cc65417be061009159e80a21
Ben Stoica <bennystoica@gmail.com>
2020-07-19 12:18:53 -0400

    added login routes and templates

--- app.js
+++ app.js
@@ -123,6 +123,19 @@ app.post('/register', function(req, res){
     });
 });
 
+app.get('/login', function(req, res){
+    res.render('login');
+})
+
+app.post('/login', passport.authenticate('local', 
+    {
+        successRedirect: '/campgrounds', 
+        failureRedirect: '/login'
+
+    }), function(req, res){
+
+});
+
 
  app.listen(3000, process.env.IP, function(){
     console.log("YelpCamp listening on Port 3000");
