728369d79f11847b73ef8ff981bc5336813158fe
Ben Stoica <bennystoica@gmail.com>
2020-07-22 19:25:14 -0400

    installed and configured flash messages

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const seedDB         = require('./seeds.js');
 const passport       = require('passport');
 const LocalStrategy  = require('passport-local');
 const User           = require('./models/user');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const flash          = require('connect-flash');
 
 //requiring routes
 const commentRoutes    = require('./routes/comments');
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
+app.use(flash());
 mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb://localhost/yelp-camp', { useUnifiedTopology: true, useNewUrlParser: true });
 
@@ -38,6 +40,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');    
     next();
 });
 
--- middleware/index.js
+++ middleware/index.js
@@ -42,11 +42,9 @@ middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
+    req.flash('error', 'You must be logged in to do this, please login');
     res.redirect('/login');
 }; 
-
-
-
 
 
 module.exports = middlewareObj;
