8529bb8eee47e0472bb346ad271718f2b6b438ba
Ben Stoica <bennystoica@gmail.com>
2020-07-24 15:01:39 -0400

    correted app breaking bug

--- middleware/index.js
+++ middleware/index.js
@@ -5,7 +5,7 @@ const middlewareObj = {};
 middlewareObj.checkCampgroundOwnership = function(req, res, next) {     
     if(req.isAuthenticated()){
         Campground.findById(req.params.id, function(err, foundCampground){
-            if(err) {
+            if(err || !foundCampground) {
                 req.flash('error', 'Campground not found');
                 res.redirect('back');
             } else {
