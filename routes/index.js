869e35f9b070b0d7295eacd4db9ed122318fe732
Ben Stoica <bennystoica@gmail.com>
2020-07-25 20:32:42 -0400

    more updates to nav bar

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res){
 
 //AUTH ROUTES
 router.get('/register', function(req, res){
-    res.render("register");
+    res.render("register", {page: 'register'});
 });
 
 router.post('/register', function(req, res){
@@ -29,7 +29,7 @@ router.post('/register', function(req, res){
 });
 
 router.get('/login', function(req, res){
-    res.render('login');
+    res.render('login', {page: 'login'});
 })
 
 router.post('/login', passport.authenticate('local', 
